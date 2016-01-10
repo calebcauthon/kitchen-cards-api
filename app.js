d3d1521b625c111151af1b7fb30b46b42fc6deda
Caleb Cauthon <calebcauthon@gmail.com>
2016-01-11 00:48:17 +1300

    Allow CORS.

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ var users = require('./routes/users');
 
 var app = express();
 
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
