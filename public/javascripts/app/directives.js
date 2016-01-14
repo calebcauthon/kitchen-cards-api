e6e7421e3239d921ac7e8cc7af99eacbd2beef85
Caleb Cauthon <calebcauthon@gmail.com>
2016-01-15 12:33:49 +1300

    Refactor

--- public/javascripts/app/directives.js
+++ public/javascripts/app/directives.js
@@ -13,37 +13,15 @@ angular.module('directives', ['k-cards-services'])
         angular.merge($scope.step, $scope.parseLine(original_text));
       };
 
-      $scope.parseLine = parseLine;
-
-      $scope.plain_text = function(step) {
-        return step.text.replace('{{verb}}', step.verb);
-      };
-
-      $scope.isStatus = function(status) {
-        return $scope.status[status] == true;
-      };
-
       $scope.reset = function() {
         $scope.status = {};
 
         if($scope.step) {
           $scope.full_text = $scope.step.text;
           $scope.split($scope.full_text);
-          $scope.changeStatus('typing', 'done');
         } else {
           $scope.full_text = '';
-          $scope.changeStatus('typing', 'typing');
         }
-      };
-
-      $scope.changeStatus = function(from, to) {
-        $scope.status[from] = false;
-        $scope.status[to] = true;
-      };
-
-      $scope.changeStatusTo = function(to) {
-        $scope.status = {};
-        $scope.changeStatus(to, to);
       };
 
       $scope.split = function(text) {
@@ -60,20 +38,20 @@ angular.module('directives', ['k-cards-services'])
         });
       };
 
-      $scope.is_measurement = function(word) {
-        if(!$scope.step.measurements)
-          $scope.step.measurements = [];
+      $scope.step_has = function(word, category) {
+        if(!$scope.step[category])
+          $scope.step[category] = [];
 
-        if($scope.step.measurements.length) {
-          return _.indexOf($scope.step.measurements, word.text) > -1;
+        if($scope.step[category].length) {
+          return _.indexOf($scope.step[category], word.text) > -1;
         } else
           return false;
       };
 
-      $scope.toggle_measurement = function(wordToToggle) {
-        $scope.step.measurements = _.chain($scope.split_text).select(function(word) {
-          return ($scope.is_measurement(word) || (word == wordToToggle)) &&
-          (!$scope.is_measurement(word) || !(word == wordToToggle))
+      $scope.step_toggle = function(wordToToggle, category) {
+        $scope.step[category] = _.chain($scope.split_text).select(function(word) {
+          return ($scope.step_has(word, category) || (word == wordToToggle)) &&
+          (!$scope.step_has(word, category) || !(word == wordToToggle))
         }).pluck('text').value();
       };
 
@@ -85,43 +63,7 @@ angular.module('directives', ['k-cards-services'])
         $scope.step.verb = wordToToggle.text;
       };
 
-      $scope.is_ingredient = function(word) {
-        if($scope.step.ingredients.length) {
-          return _.indexOf($scope.step.ingredients, word.text) > -1;
-        } else
-          return false;
-      };
-
-      $scope.toggle_ingredient = function(wordToToggle) {
-        $scope.step.ingredients = _.chain($scope.split_text).select(function(word) {
-          return ($scope.is_ingredient(word) || (word == wordToToggle)) &&
-          (!$scope.is_ingredient(word) || !(word == wordToToggle))
-        }).pluck('text').value();
-      };
-
-      $scope.is_amount = function(word) {
-        if(!$scope.step.amounts)
-          $scope.step.amounts = [];
-
-        if($scope.step.amounts.length) {
-          return _.indexOf($scope.step.amounts, word.text) > -1;
-        } else
-          return false;
-      };
-
-      $scope.toggle_amount = function(wordToToggle) {
-        $scope.step.amounts = _.chain($scope.split_text).select(function(word) {
-          return ($scope.is_amount(word) || (word == wordToToggle)) &&
-          (!$scope.is_amount(word) || !(word == wordToToggle))
-        }).pluck('text').value();
-      };
-
-      $scope.doneWithStep = function() {
-        $scope.reset();
-      };
-
       $scope.reset();
-    },
-    controllerAs: 'ctrl'
+    }
   };
 });
